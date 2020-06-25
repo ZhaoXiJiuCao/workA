71273c4ffbc49363e63a963db542fd97ff8886d6
zhaoxi  <949118128@qq.com>
2020-06-25 11:04:47 +0800

    :construction:创建项目

---
+++ emp_vue/src/main/java/com/crush/demo/EmployeeManagementApplication.java
@@ -0,0 +1,18 @@
+package com.crush.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+@RequestMapping(value="/ems")
+	public String index(){
+		return "index";
+}
+
+}
